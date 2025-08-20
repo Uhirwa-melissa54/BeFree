8e230eabe0f713aa646d27867217030349ea7765
Uwase Sonia <uwasesonia43@example.com>
2025-08-20 18:40:32 +0200

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -12,11 +12,11 @@ router.post("/", async(req,res) => {
         return res.status(409).send({message: "User with given email already exists"})
     const salt=await bcrypt.genSalt(Number(process.env.SALT))
     const hashPassword=await bcrypt.hash(req.body.password,salt)
-    await new User({...req.body, password: hashPassword}).save
-    req.status(201).send({message: "User created successfully"})
+    await new User({...req.body, password: hashPassword}).save()
+    res.status(201).send({message: "User created successfully"})
     }
     catch(err) {
         res.status(500).send({message: "Internal Server Error"})
     }
 })
-module.exports.router
+module.exports=router
